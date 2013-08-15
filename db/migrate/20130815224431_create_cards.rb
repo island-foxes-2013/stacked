20c80cb4c6149c9d9786195e6c2f48b7c3d2ba80
Mitch Seaman <mitchel.seaman@gmail.com>
2013-08-15 15:53:20 -0700

    card migration

--- db/migrate/20130815224431_create_cards.rb
+++ db/migrate/20130815224431_create_cards.rb
@@ -1,6 +1,9 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
+    	t.string :name
+    	t.string :twitter_handle
+    	t.belongs_to :board
 
       t.timestamps
     end
