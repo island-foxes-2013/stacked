3980a7dbb3f9610dc24e1d04161c7510625e8d28
Daniel Willems <dmkwillems@gmail.com>
2013-08-15 21:42:07 -0700

    git wouldn't let me checkout with adding these changes.  Didn't see any

--- spec/factories/authorization_factory.rb
+++ spec/factories/authorization_factory.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: authorizations
+#
+#  id         :integer          not null, primary key
+#  provider   :string(255)
+#  uid        :string(255)
+#  user_id    :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do 
 
 	factory :authorization do 
