5cb06a6482cb5235627240c5ebc1645d24a0c4cc
Eric Savvy <eric.chen0121@gmail.com>
2013-08-15 21:17:03 -0700

    added seed file for isl foxes - cards and a board

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Card.create(name: 'Eric Chen', twitter_handle: 'ericchen0121')
-Card.create(name: 'Adam Loo', twitter_handle: 'adamloo2')
-Card.create(name: 'Greg Varias', twitter_handle: '180AD')
-Card.create(name: 'Dan Willems', twitter_handle: 'dmkwillems')
-Card.create(name: 'Clint Mullins', twitter_handle: 'clintfmullins')
+islfox = Board.create(name: 'DBC Island Foxes', description: 'Island fox cohort, Graduating Aug 2013.')
 
-Board.create()
+islfox_cards = []
+islfox_cards << Card.create(name: 'Eric Chen', twitter_handle: 'ericchen0121')
+islfox_cards << Card.create(name: 'Adam Loo', twitter_handle: 'adamloo2')
+islfox_cards << Card.create(name: 'Greg Varias', twitter_handle: '180AD')
+islfox_cards << Card.create(name: 'Dan Willems', twitter_handle: 'dmkwillems')
+islfox_cards << Card.create(name: 'Clint Mullins', twitter_handle: 'clintfmullins')
+
+islfox_cards.each do |fox|
+  islfox.cards << fox
+end
