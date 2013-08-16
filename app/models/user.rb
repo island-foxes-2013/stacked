2aec35274afc69ec7272d86c0eddbde6773fbbbc
Mitch Seaman <mitchel.seaman@gmail.com>
2013-08-16 16:28:16 -0700

    omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,21 @@
 #
 #  id         :integer          not null, primary key
 #  name       :string(255)
+#  image_url  :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :image_url
 
   validates_presence_of :name
 
   has_many :authorizations
 
   def self.create_from_hash!(hash)
-  	create(name: hash['info']['name'])
+  	ap hash
+  	create(name: hash['info']['name'], image_url: hash['info']['image'])
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20130816033323) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
+    t.string   "image_url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
