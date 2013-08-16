18d36b7fb31de9c7f3ccf22cae14d310e359c3bb
Clint Mullins <clintfmullins@gmail.com>
2013-08-16 16:04:51 -0700

    git puuuushing some chhhhanges (no more card columns)

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -24,33 +24,33 @@ $(document).ready(function() {
   });
 
   $(window).resize(function() {
-    loadCards();
+    //loadCards();
     $('.flip').fadeIn(600);
     $('.card').addClass('flipped');
   });
 });
 
 
-function loadCards(){
-  var windowSize = $(window).width();
-  console.log(windowSize);
+// function loadCards(){
+//   var windowSize = $(window).width();
+//   console.log(windowSize);
 
-  if (windowSize <= 666){
-    $('.flip').removeClass().addClass('flip large-10 columns');
-  }
-  else if (windowSize <= 767){
-    $('.flip').removeClass().addClass('flip small-5 columns');
-  }
-  else if (windowSize <= 1000){
-    $('.flip').removeClass().addClass('flip large-4 columns');
-  }
-  else if (windowSize <= 1500){
-    $('.flip').removeClass().addClass('flip large-3 columns');
-  }
-  else if (windowSize > 1500) {
-    $('.flip').removeClass().addClass('flip large-2 columns');
-  }
-}
+//   // if (windowSize <= 666){
+//   //   $('.flip').removeClass().addClass('flip large-10 columns');
+//   // }
+//   // else if (windowSize <= 767){
+//   //   $('.flip').removeClass().addClass('flip small-5 columns');
+//   // }
+//   // else if (windowSize <= 1000){
+//   //   $('.flip').removeClass().addClass('flip large-4 columns');
+//   // }
+//   // else if (windowSize <= 1500){
+//   //   $('.flip').removeClass().addClass('flip large-3 columns');
+//   // }
+//   // else if (windowSize > 1500) {
+//   //   $('.flip').removeClass().addClass('flip large-2 columns');
+//   // }
+// }
 
 
 
