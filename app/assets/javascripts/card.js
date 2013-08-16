41df241cca7c444825093788d2b002b435ac7e36
Clint Mullins <clintfmullins@gmail.com>
2013-08-15 23:10:53 -0700

    cleaned up card comments

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	$(document).on('mouseover','.card', function(){
-    $(document).find('.card').addClass('flipped').mouseleave(function(){
+	$(document).on('mouseover','.flip', function(){
+    $(this).find('.card').addClass('flipped').mouseleave(function(){
         $(this).removeClass('flipped');
     });
     return false;
