aa219a59ea8707eb8fe8268419348d96c6d167e8
Eric Savvy <eric.chen0121@gmail.com>
2013-08-18 01:46:24 -0700

    the rails way for linking to Boards through the Ransack gem

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,6 +5,7 @@ class BoardsController < ApplicationController
 
     # Ransack gem search objects
     @search = Board.search(params[:q])
+    # will resturn a list of objects
     @search_suggestions = @search.result
 	end
 
