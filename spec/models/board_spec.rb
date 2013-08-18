24867fe43e108fb2e014f8ce9da69112ea0a915b
Clint Mullins <clintfmullins@gmail.com>
2013-08-17 19:04:47 -0700

    model changes

--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -6,7 +6,6 @@
 #  name        :string(255)
 #  description :string(255)
 #  slug        :string(255)      not null
-#  user_id     :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
