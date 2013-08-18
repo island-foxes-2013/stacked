57aa72a80afc4b387a1ccbdfd4f17ee0463f6c7d
Clint Mullins <clintfmullins@gmail.com>
2013-08-18 03:10:18 -0700

    edit is now on the page

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function(){
+	$('.board-edit').on('click', function(){
+		$('.inline-edit').slideToggle();
+		$('.delete').slideToggle();
+	});
+});
