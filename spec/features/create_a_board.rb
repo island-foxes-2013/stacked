2aabcc8b91d35216dee87b61c1339724e33b862c
Daniel Willems <dmkwillems@gmail.com>
2013-08-18 11:05:50 -0700

    created test for link for new board and creating a board

--- spec/features/create_a_board.rb
+++ spec/features/create_a_board.rb
@@ -1,7 +1,31 @@
 require 'spec_helper'
 
-	desscribe 'board' do 
+	#current tests do no require logging in
+	describe 'board' do 
 
-		
+		context "creating one board" do
+
+			it "should have a link to create a new board on home page" do
+				visit root_path
+				page.should have_content('New Board')
+			end
+	
+			it "should have field for name" do
+				pending
+			end
+
+			it "should have field for description" do
+				pending
+			end
+
+			it "creates a board" do
+    		visit '/boards/new'
+      	fill_in 'board_name', :with => 'Cool'
+      	fill_in 'board_description', :with => 'This is a cool test board'
+    		click_button 'Create'
+    		expect(page).to have_content 'Cool'
+  		end
+
+		end
 		
 	end
