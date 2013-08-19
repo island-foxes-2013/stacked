f2dd6e1392a2e5976d6455d966dd342f9f90f855
Eric Savvy <eric.chen0121@gmail.com>
2013-08-18 20:20:08 -0700

    removed instance variables in Searches controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,12 +11,12 @@ class SearchesController < ApplicationController
     if q
       @boards_name_results = Board.search(name_cont: q).result 
 
-      @cards_name_results = Card.search(name_cont: q).result
-      # @cards_name_results = Card.search(name_cont_or_twitter_handle_cont: q).result
-      @cards_twitter_results = Card.search(twitter_handle_cont: q).result
-      @cards_instagram_results = Card.search(instagram_handle_cont: q).result
+      cards_name_results = Card.search(name_cont: q).result
+      cards_twitter_results = Card.search(twitter_handle_cont: q).result
+      cards_instagram_results = Card.search(instagram_handle_cont: q).result
 
-      @cards = (@cards_name_results + @cards_twitter_results + @cards_instagram_results).uniq
+      @cards = (cards_name_results + cards_twitter_results + cards_instagram_results).uniq
+
     end
 
   end
