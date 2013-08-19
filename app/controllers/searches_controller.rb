525d0c61fc8f012cbe43b4dfc4eaf85a0e3625e4
Eric Savvy <eric.chen0121@gmail.com>
2013-08-18 18:03:11 -0700

    search by instagram handle

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,9 +3,10 @@ class SearchesController < ApplicationController
   def index
     # Ransack gem query
     q = params[:q]
-    @boards_searchresults = Board.search(name_cont: q).result
-    @cards_searchresults = Card.search(name_cont: q).result
-    @cards_twitterhandle_searchresults = Card.search(twitter_handle_cont: q).result
+    @boards_name_results = Board.search(name_cont: q).result
+    @cards_name_results = Card.search(name_cont: q).result
+    @cards_twitter_results = Card.search(twitter_handle_cont: q).result
+    @cards_instagram_results = Card.search(instagram_handle_cont: q).result
   end
 
 end
