31df24376bda9cf974323c94ac2a81c340051277
Eric Savvy <eric.chen0121@gmail.com>
2013-08-18 17:53:10 -0700

    search by twitter handle with Ransack gem

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,10 +5,7 @@ class SearchesController < ApplicationController
     q = params[:q]
     @boards_searchresults = Board.search(name_cont: q).result
     @cards_searchresults = Card.search(name_cont: q).result
-    @users_searchresults = User.search(name_cont: q).result
-
-    # TODO NOT WORKING YET - look into Ransack cont helper
-    # @cards_twitterhandle_searchresults = Card.search(twiter_handle_cont: q).result
+    @cards_twitterhandle_searchresults = Card.search(twitter_handle_cont: q).result
   end
 
 end
