24a42131ae16e504db70d330466e2ea62243b0d5
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-08-19 11:07:08 -0700

    first-pass refactor of card.js; moved templates into their own partial

--- app/assets/javascripts/card.js
+++ app/assets/javascripts/card.js
@@ -1,4 +1,132 @@
+var TweetFormat = {
+  new: function(tweet) {
+    var $post = $(".templates").find(".text-post");
+    $post.find(".content").html(tweet.text);
+    $post.find("a").attr("href", "https://twitter.com/"+tweet.user_id+"/statuses/"+tweet.tweet_id);
+    // TODO-JW: We shouldn't have to return .html() here.
+    return $post.html();
+  }
+};
+
+var InstaFormat = {
+  new: function(instagram) {
+    var $post = $(".templates").find(".image-post");
+    $post.find(".content").text(instagram.text);
+    $post.find(".source").find("img").attr("src");
+    return $post;
+  }
+};
+
+var LazyLoader = {
+  init: function() {
+    var self = this;
+    $('.update-twitter').on("ajax:success", function(event, xhr, status, error){
+      console.log(xhr)
+      htmlString = ''
+      for (i in xhr) {
+        $(this).closest('.face.back').find('.news').append(TweetFormat.new(xhr[i]));
+        //htmlString += TweetFormat.new(xhr[i])
+      }
+      //$(this).closest('.face.back').find('.news').append(htmlString);
+      $(this).closest('.card').addClass('flipped');
+      self.handleExternalLinks();
+    });
+    $('.update-twitter').on("ajax:error", function(event, xhr, status, error){
+      console.log('errrrrrrorr')
+      $(this).closest('.card').addClass('flipped');
+      self.handleExternalLinks();
+    });
+
+    $(document).on('ajax:success','.delete', function(event, xhr, status, error) {
+      console.log(event)
+      $('#container').isotope('remove', $(event.target).closest('.card-wrapper'));
+      // $(this).closest('.flip').fadeOut('slow');  
+    }); 
+  },
+
+  handleExternalLinks: function() {
+    $("a[href^=http]").each(function(){
+      if(this.href.indexOf(location.hostname) == -1) {
+         $(this).attr({
+            target: "_blank",
+            title: "Opens in a new window"
+         });
+      }
+    });
+  },
+
+  load: function() {
+    $('.update-twitter').click();
+  }
+};
+
+var DragDrop = {
+  init: function() {
+    var self = this;
+    $(window).resize(function() {
+      // loadCards();
+      // $('.card').addClass('flipped');
+      self.makeCardsDraggable();
+      self.makeDecksDroppable();
+      $('.flip').fadeIn(600);
+    });    
+  },
+
+  makeCardsDraggable: function() {
+    $(".card").find(".header").draggable({
+      helper    : function(e){
+
+        var $card = $(e.target);
+        var $dragBuddy = $('.dragged-card').clone();
+
+        var imgUrl = $card.find('.mini-pic').attr('id').toString();
+        var cardId = $card.closest('.card').attr("id");
+
+        $dragBuddy.find('.name').text($card.find('.name').text());
+
+        $dragBuddy.find('.profile-pic').attr('src',imgUrl);
+        $dragBuddy.attr("id",cardId);
+
+        // console.log($dragBuddy);
+        return $dragBuddy[0];
+      },
+      // TODO-JW: figure out positioning
+      cursor    : 'move',
+      cursorAt  : { left : 5 },
+      appendTo  : 'body',
+      zIndex    : 999
+    });
+  },
+
+  makeDecksDroppable: function() {
+    $(".board-link").droppable({
+      drop        : this.addCardToDeck,
+      hoverClass  : 'drop-hover'
+    });
+  },
+
+  addCardToDeck: function(event, ui) {
+    var card = ui.draggable.closest('.card');
+    console.log(card);
+    var board = $(this);
+    console.log( 'The square with ID "' + card.attr('id') + '" was dropped onto ' + board.attr('id'));
+    $.ajax({
+      url: '/board_cards',
+      method: 'post',
+      data: {board_slug: board.attr('id'), card_slug: card.attr('id')}
+    }).done(function(data) {
+      console.log(data)
+    });
+  }
+};
+
+
 $(document).ready(function() {
+  LazyLoader.init();
+  DragDrop.init();
+
+  LazyLoader.load();
+
   // $('.frosty').blurjs({
   //   source: 'body',
   //   radius: 10,
@@ -12,45 +140,12 @@ $(document).ready(function() {
   //   return false;
   // });
 
-  $('.update-twitter').on("ajax:success", function(event, xhr, status, error){
-    console.log(xhr)
-    htmlString = ''
-    for (i in xhr) {
-      htmlString += tweet(xhr[i])
-    }
-    $(this).closest('.face.back').find('.news').append(htmlString);
-    $(this).closest('.card').addClass('flipped');
-    handleExternalLinks();
-  });
-  $('.update-twitter').on("ajax:error", function(event, xhr, status, error){
-    console.log('errrrrrrorr')
-    $(this).closest('.card').addClass('flipped');
-    handleExternalLinks();
-  });
-
-  $(document).on('ajax:success','.delete', function(event, xhr, status, error) {
-    console.log(event)
-    $('#container').isotope('remove', $(event.target).closest('.card-wrapper'));
-    // $(this).closest('.flip').fadeOut('slow');  
-  });
-
-  $('.update-twitter').click();
-    
   // $(document).on('click','.card.flipped',function(){
   //   console.log(this);
   //   var handle = '@CrabCaker'
   //   var content = 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Pellentesque in erat cras amet.'
   //   $(this).find('.face.back').append(tweetFormat(handle,content));
   // });
-
-  $(window).resize(function() {
-    // loadCards();
-    // $('.card').addClass('flipped');
-    makeCardsDraggable();
-    makeDecksDroppable();
-    $('.flip').fadeIn(600);
-  });
-
 });
 
 // function loadCards(){
@@ -78,93 +173,3 @@ $(document).ready(function() {
 //     $('.flip').css('width', '18%')
 //   }
 // }
-
-function tweet(tweet){
-  return tweetFormat(tweet.tweet_id, tweet.text, tweet.user_id)
-}
-
-function tweetFormat(tweet_id, text, user_id) {
-  return "<div class='tweet'>"+
-            "<div class='content'>" +
-              text +
-            "</div>" +
-            "<a href='https://twitter.com/"+user_id+"/statuses/"+tweet_id+"'>"+
-              "<img alt='t' src='/assets/nottwittersbird.png'>" +
-            "</a>"+
-          "</div>"
-}
-
-function instaFormat(instagram) {
-  var $post = $(".templates").find(".image_post");
-  $post.find(".content").text(instagram.text);
-  $post.find(".source").find("img").attr("src");
-  return $post;
-}
-
-// function tweetPicture(tweet_id, content, picUrl) {
-//   return "<div class='tweet'>"+
-//             "<img border='0' src='http://i.stack.imgur.com/skvx1.png' width='200' height='200'>" +
-//             "<div class='content'>" +
-//               content +
-//             "</div>" +
-//           "</div>"
-// }
-
-function handleExternalLinks() {
- $("a[href^=http]").each(function(){
-    if(this.href.indexOf(location.hostname) == -1) {
-       $(this).attr({
-          target: "_blank",
-          title: "Opens in a new window"
-       });
-    }
- });
-}
-
-function makeCardsDraggable() {
-  $(".card").find(".header").draggable({
-    helper    : function(e){
-
-      var $card = $(e.target);
-      var $dragBuddy = $('.dragged-card').clone();
-
-      var imgUrl = $card.find('.mini-pic').attr('id').toString();
-      var cardId = $card.closest('.card').attr("id");
-
-      $dragBuddy.find('.name').text($card.find('.name').text());
-
-      $dragBuddy.find('.profile-pic').attr('src',imgUrl);
-      $dragBuddy.attr("id",cardId);
-
-      // console.log($dragBuddy);
-      return $dragBuddy[0];
-    },
-    cursor    : 'move',
-    cursorAt  : { left : 5 },
-    appendTo  : 'body',
-    zIndex    : 999
-  });
-}
-
-function makeDecksDroppable() {
-  $(".board-link").droppable({
-    drop        : addCardToDeck,
-    hoverClass  : 'drop-hover'
-  })
-}
-
-function addCardToDeck(event,ui) {
-  var card = ui.draggable.closest('.card');
-  console.log(card);
-  var board = $(this);
-  console.log( 'The square with ID "' + card.attr('id') + '" was dropped onto ' + board.attr('id'));
-  $.ajax({
-    url: '/board_cards',
-    method: 'post',
-    data: {board_slug: board.attr('id'), card_slug: card.attr('id')}
-  }).done(function(data) {
-    console.log(data)
-  })
-}
-
-
