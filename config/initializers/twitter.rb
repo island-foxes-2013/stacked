b4df0f0df571ec9bfe70d5d1d20258773b58d9a6
Daniel Willems <dmkwillems@gmail.com>
2013-08-20 17:32:04 -0700

    working with Zee. made follow link tests

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -10,7 +10,3 @@ Twitter.configure do |config|
 end
 
 # Then, for each user's access token/secret pair, instantiate a Twitter::Client:
-eric  = Twitter::Client.new(
-  oauth_token: '17861518-NUVAfsalQsBujIPPoGq4kgZM3SPwdJj1OAwXDGC7i',
-  oauth_token_secret: 'Vg7bMv9bJuwhfgcFCa7LgNpC8Sj8YUI3nrfIM5kFJM'
-)
