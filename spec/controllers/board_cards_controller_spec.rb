cc7b374f2c4a8427ccb4a2f9a7c03925b8c0b2ad
Daniel Willems <dmkwillems@gmail.com>
2013-08-20 18:00:48 -0700

    added factories

--- spec/controllers/board_cards_controller_spec.rb
+++ spec/controllers/board_cards_controller_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 describe BoardCardsController do
 
+	let(:board) { FactoryGirl.create(:board) }
+	let(:user) { FactoryGirl.create(:user) }
 
 
+		describe "#create" do
 
+		end
+
+		describe "#destroy" do
+
+
+		end
 
 end
