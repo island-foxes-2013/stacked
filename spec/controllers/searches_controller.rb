0a40efbe22198f3af35ada0bfc33aea05955f27b
Daniel Willems <dmkwillems@gmail.com>
2013-08-20 22:48:17 -0700

    added routes

--- spec/controllers/searches_controller.rb
+++ spec/controllers/searches_controller.rb
@@ -4,30 +4,30 @@ describe SearchesController do
 
 	describe '#index' do
 		it "returns user results " do
-			
+			get :index
 		end
 
 		it "returns twitter results " do
+			get :index
 		end
 	
 		it "returns instagram results " do
+			get :index
 		end
-
 	end
 
 	describe '#global' do
 		it "returns user results " do
-			
+			get :global
 		end
 
 		it "returns twitter results " do
+			get :global
 		end
-	
+
 		it "returns instagram results " do
+			get :global
 		end
 
-		
 	end
-
-
 end
