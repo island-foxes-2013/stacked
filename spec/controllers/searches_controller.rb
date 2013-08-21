c0352e41fc1e4e3303e86dc3ea8cb629efd1ae83
Daniel Willems <dmkwillems@gmail.com>
2013-08-20 22:45:58 -0700

    added search skelton

---
+++ spec/controllers/searches_controller.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe SearchesController do
+
+	describe '#index' do
+		it "returns user results " do
+			
+		end
+
+		it "returns twitter results " do
+		end
+	
+		it "returns instagram results " do
+		end
+
+	end
+
+	describe '#global' do
+		it "returns user results " do
+			
+		end
+
+		it "returns twitter results " do
+		end
+	
+		it "returns instagram results " do
+		end
+
+		
+	end
+
+
+end
