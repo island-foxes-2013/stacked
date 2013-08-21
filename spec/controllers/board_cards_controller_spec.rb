4a4d432b0bfec853ba3af08c7436a43128265d47
Daniel Willems <dmkwillems@gmail.com>
2013-08-20 18:02:57 -0700

    card board controllers defined

--- spec/controllers/board_cards_controller_spec.rb
+++ spec/controllers/board_cards_controller_spec.rb
@@ -7,12 +7,15 @@ describe BoardCardsController do
 
 
 		describe "#create" do
-
+			it "board card relationship should be created" do
+			
+			end
 		end
 
 		describe "#destroy" do
+			it "board card relationship should be destroy" do
 
-
+			end
 		end
 
 end
